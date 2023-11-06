3d70fb7a75b0dcdf256790303720dc3f2b2e0071
rmungar <mgraul@iesrafaelalberti.es>
2023-11-06 21:34:04 +0100

    a

--- src/Ejercicio2_3_5.py
+++ src/Ejercicio2_3_5.py
@@ -2,7 +2,7 @@ contraseña_guardada = "rmungar1209"
 try:
     contraseña_introducida = input("Ingrese la contraseña: ")
     if contraseña_guardada != contraseña_introducida:
-        raise ValueError("Incorrect Password!!")
+        raise NameError("Incorrect Password!!")
     print("WELCOME!!")
-except ValueError as e:
+except NameError as e:
     print(e)
